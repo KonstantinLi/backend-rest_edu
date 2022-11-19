1cfeec67fa1b6c1bfb552826fa1a4788818d4603
linko <linenkokonstantin@gmail.com>
2022-11-19 17:39:30 +0200

    schemas for entities user, category, record

---
+++ app/schemas.py
@@ -0,0 +1,19 @@
+from marshmallow import Schema, fields
+
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+
+
+class CategorySchema(Schema):
+    id = fields.Int(dump_only=True)
+    type = fields.Str(required=True)
+
+
+class RecordSchema(Schema):
+    id = fields.Int(dump_only=True)
+    user_id = fields.Int(required=True)
+    category_id = fields.Int(required=True)
+    pay = fields.Int(required=True)
+    date_time = fields.DateTime()
