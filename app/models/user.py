a859d42280cf5d19ca38d20912d74481884d4d6b
KonstantinLi <linenkokonstantin@gmail.com>
2023-01-05 20:37:19 +0200

    modernize UserModel and UserSchema

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,8 @@ class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
     currency_id = db.Column(db.Integer, db.ForeignKey("currency.id"), nullable=False, default=1)
 
     currency = db.relationship("CurrencyModel", back_populates="users")
--- app/schemas.py
+++ app/schemas.py
@@ -2,9 +2,10 @@ from marshmallow import Schema, fields
 
 
 class UserSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True)
     currency_id = fields.Int(required=False)
+    access_token = fields.Str(required=False)
 
 
 class CategorySchema(Schema):
