2f01e0533b8e9d89e5e484ccbe589162540fbc27
KonstantinLi <linenkokonstantin@gmail.com>
2023-01-05 20:35:36 +0200

    flask-jwt-extended bootstrap

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
+import os
+
+from flask import jsonify
+
 from app import app
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 from .db import db
 from .models.currency import CurrencyModel
 from .resources.category import blp as CategoryBlueprint
@@ -14,9 +19,44 @@ app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 db.init_app(app)
 
 api = Api(app)
+
+jwt = JWTManager(app)
+
+
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+    return (
+        jsonify({"message": "The token has expired.", "error": "token_expired"}),
+        401,
+    )
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return (
+        jsonify(
+            {"message": "Signature verification failed.", "error": "invalid_token"}
+        ),
+        401,
+    )
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return (
+        jsonify(
+            {
+                "description": "Request does not contain an access token.",
+                "error": "authorization_required",
+            }
+        ),
+        401,
+    )
+
 
 with app.app_context():
     # Опціонально
@@ -31,7 +71,6 @@ with app.app_context():
         for currency in [currency1, currency2, currency3]:
             db.session.add(currency)
         db.session.commit()
-
 
 api.register_blueprint(UserBlueprint)
 api.register_blueprint(CategoryBlueprint)
